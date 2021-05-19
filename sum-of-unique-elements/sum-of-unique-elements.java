6a8dbfeb94354cf6e576c8644d3673e305fd14e9
Dipjul <dipjulrahman2@gmail.com>
2021-05-20 02:40:16 +0530

    Update sum-of-unique-elements.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Find N Unique Integers Sum up to Zero.
    Memory Usage: 37 MB, less than 83.24% of Java online submissions for Find N Unique Integers Sum up to Zero.

--- sum-of-unique-elements/sum-of-unique-elements.java
+++ sum-of-unique-elements/sum-of-unique-elements.java
@@ -15,3 +15,44 @@ class Solution {
         return sum;
     }
 }
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        int start = 1;
+        if(n%2 == 0) {
+            for(int i=n/2-1,j=n/2;i>=0;i--,j++) {
+                res[i] = start++;
+                res[j] = res[i]*-1;
+            }
+        } 
+        else {
+            res[n/2] = 0;
+            for(int i=n/2-1,j=n/2+1;i>=0;i--,j++) {
+                res[i] = start++;
+                res[j] = res[i]*-1;
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] arr = new int[101];
+        int sum = 0;
+        for(int i=0;i<nums.length;i++) {
+            if(arr[nums[i]] == 0){
+                arr[nums[i]] = 1;
+                sum+=nums[i];
+            }
+            else{
+                if(arr[nums[i]] == 1)
+                    sum-=nums[i];
+                arr[nums[i]] = 2;
+                
+            }
+        }
+        return sum;
+    }
+}
