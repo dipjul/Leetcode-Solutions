e9ecacccf75f7ea40507ff9a079f33be203319ba
Dipjul <dipjul25@gmail.com>
2021-10-22 10:42:16 +0530

    Time: 2 ms (45.78%), Space: 41.8 MB (34.05%) - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -6,11 +6,12 @@ class Solution {
         for(int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
             windowSum += nums[windowEnd];
             while(windowSum >= target) {
-                windowSum -= nums[windowStart];
                 min = Math.min(min, windowEnd - windowStart + 1);
+                windowSum -= nums[windowStart];
                 windowStart++;
             }
+            
         }
-        return min<Integer.MAX_VALUE?min:0;
+        return min < Integer.MAX_VALUE? min:0;
     }
 }
