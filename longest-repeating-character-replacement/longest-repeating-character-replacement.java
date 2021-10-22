949bf4750133e2ce154e2d3ef6f41466efba8b5d
Dipjul <dipjul25@gmail.com>
2021-10-22 17:54:40 +0530

    Time: 26 ms (20.32%), Space: 42.4 MB (5.75%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -2,19 +2,23 @@ class Solution {
     public int characterReplacement(String s, int k) {
         int windowStart = 0, maxLength = 0, mostRepeatingCharCount = 0;
         
-        HashMap<Character, Integer> mp = new HashMap();
+        HashMap<Character, Integer> charFreqMap = new HashMap();
         
-        for(int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
-            char right = s.charAt(windowEnd);
-            mp.put(right, mp.getOrDefault(right, 0)+1);
-            mostRepeatingCharCount = Math.max(mostRepeatingCharCount, mp.get(right));
-            if(windowEnd - windowStart + 1 - mostRepeatingCharCount > k) {
+        for(int wE = 0; wE < s.length(); wE++) {
+            char right = s.charAt(wE);
+            charFreqMap.put(right, charFreqMap.getOrDefault(right, 0)+1);
+            mostRepeatingCharCount = Math.max(mostRepeatingCharCount, charFreqMap.get(right));
+            
+            while(wE-windowStart+1 - mostRepeatingCharCount > k) {
                 char left = s.charAt(windowStart);
-                mp.put(left, mp.get(left)-1);
+                charFreqMap.put(left, charFreqMap.get(left)-1);
+                if(charFreqMap.get(left) == 0)
+                    charFreqMap.remove(left);
                 windowStart++;
             }
-            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+            maxLength = Math.max(maxLength, wE-windowStart+1);
         }
+        
         return maxLength;
     }
 }
