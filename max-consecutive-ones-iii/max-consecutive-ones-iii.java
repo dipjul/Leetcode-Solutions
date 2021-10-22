c64105569532df8515a7224d292bba2758cd25e3
Dipjul <dipjul25@gmail.com>
2021-10-22 22:44:29 +0530

    Time: 7 ms (11.20%), Space: 68.8 MB (18.76%) - LeetHub

--- max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,20 +1,17 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int windowStart = 0, zeroCountInWindow = 0, maxLen = 0;
-        HashMap<Integer, Integer> mp = new HashMap();
+        int windowStart = 0, onesCount = 0, maxLen = 0;
         
         for(int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
-            int right = nums[windowEnd];
-            if(right == 0) {
-                mp.put(right, mp.getOrDefault(right, 0)+1);
-            }
-            while((mp.containsKey(0)?mp.get(0):0) > k) {
-                if(nums[windowStart] == 0)
-                    mp.put(0, mp.get(0)-1);
+            if(nums[windowEnd] == 1) onesCount++;
+            while(windowEnd - windowStart + 1 - onesCount > k) {
+                if(nums[windowStart] == 1)
+                    onesCount--;
                 windowStart++;
             }
-            maxLen = Math.max(maxLen, windowEnd - windowStart + 1);  
+            maxLen = Math.max(maxLen, windowEnd - windowStart + 1);
         }
+        
         return maxLen;
     }
 }
