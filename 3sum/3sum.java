29027f404bdb28e477054d41f7923f4f787060a6
Dipjul <dipjul25@gmail.com>
2021-10-26 09:23:12 +0530

    Time: 36 ms (46.93%), Space: 56.4 MB (37.33%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> triplets = new ArrayList();
-        
+        List<List<Integer>> result = new ArrayList();
         for(int i = 0; i < nums.length-2; i++) {
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            search(nums, -nums[i], i+1, triplets);
+            search(nums, -nums[i], i+1, result);
         }
-        return triplets;
+        return result;
     }
     
-    private void search(int[] arr, int targetSum, int left, List<List<Integer>> triplets) {
+    private void search(int[] arr, int targetSum, int left, List<List<Integer>> result) {
         int right = arr.length-1;
         
         while(left < right) {
-            int currentSum = arr[left]+arr[right];
-            if(currentSum == targetSum) {
-                triplets.add(Arrays.asList(-targetSum, arr[left], arr[right]));
+            int sum = arr[right] + arr[left];
+            
+            if(sum == targetSum) {
+                result.add(Arrays.asList(-targetSum, arr[left], arr[right]));
                 left++;
                 right--;
                 while(left < right && arr[left] == arr[left-1])
                     left++;
                 while(left < right && arr[right] == arr[right+1])
                     right--;
-            } else if(targetSum > currentSum)
+            } else if(sum < targetSum) {
                 left++;
-            else
+            } else
                 right--;
         }
     }
