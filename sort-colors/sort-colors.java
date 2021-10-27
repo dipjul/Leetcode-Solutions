2fe7059aeeca737e877c3c9c95e21159b9993a6d
Dipjul <dipjul25@gmail.com>
2021-10-27 10:12:23 +0530

    Time: 0 ms (100.00%), Space: 37.8 MB (50.57%) - LeetHub

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -1,7 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i = 0, lowBound=0, highBound = nums.length - 1;
-        
+        int lowBound = 0, highBound = nums.length-1;
+        int i = 0;
         while(i <= highBound) {
             if(nums[i] == 0) {
                 swap(nums, i, lowBound);
@@ -10,14 +10,15 @@ class Solution {
             } else if(nums[i] == 2) {
                 swap(nums, i, highBound);
                 highBound--;
-            } else
+            } else {
                 i++;
+            }
         }
     }
     
-    private void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
