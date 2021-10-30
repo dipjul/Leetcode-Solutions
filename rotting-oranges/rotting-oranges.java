c6da6382e3556d44a5b7fa7b077868c83270f8e5
Dipjul <dipjul25@gmail.com>
2021-10-30 18:06:04 +0530

    Time: 3 ms (68.52%), Space: 38.9 MB (51.02%) - LeetHub

--- rotting-oranges/rotting-oranges.java
+++ rotting-oranges/rotting-oranges.java
@@ -1,50 +1,49 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0)
-            return 0;
-        
         int rows = grid.length;
         int cols = grid[0].length;
         
-        int fresh_count = 0;
-        
         Queue<int[]> rottens = new LinkedList<>();
+        int count_fresh = 0;
         
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(grid[i][j] == 2)
-                    rottens.offer(new int[]{i, j});
+                    rottens.offer(new int[] {i, j});
                 if(grid[i][j] == 1)
-                    fresh_count++;
+                    count_fresh++;
             }
         }
         
-        if(fresh_count == 0)
+        if(count_fresh == 0)
             return 0;
         
         int count = 0;
+        
         int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
         
         while(!rottens.isEmpty()) {
             count++;
             int size = rottens.size();
+            
             for(int i = 0; i < size; i++) {
                 int[] point = rottens.poll();
-                
+            
                 for(int dir[]:dirs) {
-                    int x = point[0] + dir[0];
-                    int y = point[1] + dir[1];
-                    
-                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2)
+                    int x = point[0]+dir[0];
+                    int y = point[1]+dir[1];
+
+                    if(x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] == 0 || grid[x][y] == 2)
                         continue;
                     grid[x][y] = 2;
                     rottens.offer(new int[]{x, y});
-                    fresh_count--;
+                    count_fresh--;
                 }
             }
+            
+            
         }
         
-        return fresh_count==0?count-1:-1;
-        
+        return count_fresh == 0? count-1:-1;
     }
 }
