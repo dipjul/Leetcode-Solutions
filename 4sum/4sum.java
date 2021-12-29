b2395af435b992211a7d4d895f0b14626b8d75ff
Dipjul <dipjul25@gmail.com>
2021-12-29 10:08:20 +0530

    Time: 17 ms (51.15%), Space: 39.5 MB (63.79%) - LeetHub

--- 4sum/4sum.java
+++ 4sum/4sum.java
@@ -1,38 +1,35 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        
-        List<List<Integer>> quads = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList();
         
         Arrays.sort(nums);
+        
         for(int i = 0; i < nums.length-3; i++) {
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            
             for(int j = i + 1; j < nums.length-2; j++) {
                 if(j > i + 1 && nums[j] == nums[j-1])
                     continue;
-                search(nums, target, i, j, quads);
+                search(nums, target, i, j, result);
             }
         }
-        return quads;
+        return result;
     }
     
-    private void search(int[] arr, int target, int first, int second, List<List<Integer>> quads) {
-        int left = second+1;
-        int right = arr.length-1;
-        
+    private void search(int[] nums, int target, int first, int second, List<List<Integer>> result) {
+        int left = second + 1, right = nums.length-1;
         while(left < right) {
-            int sum = arr[first] + arr[second] + arr[left] + arr[right];
-            
+            int sum = nums[first] + nums[second] + nums[left] + nums[right];
             if(sum == target) {
-                quads.add(Arrays.asList(arr[first], arr[second], arr[left], arr[right]));
+                result.add(Arrays.asList(nums[first], nums[second], nums[left], nums[right]));
                 left++;
                 right--;
-                while(left < right && arr[left] == arr[left-1])
+                while(left < right && nums[left]==nums[left-1])
                     left++;
-                while(left < right && arr[right] == arr[right+1])
+                while(left < right && nums[right]==nums[right+1])
                     right--;
-            } else if(sum < target) {
+            }
+            else if(sum < target) {
                 left++;
             } else {
                 right--;
