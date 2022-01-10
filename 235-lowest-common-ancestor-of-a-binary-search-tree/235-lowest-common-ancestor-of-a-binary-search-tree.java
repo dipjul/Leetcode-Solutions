9253ebd421b2319f89322d18e6b3838e1fee901e
Dipjul <dipjul25@gmail.com>
2022-01-10 23:45:12 +0530

    Time: 11 ms (7.40%), Space: 46.7 MB (30.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -11,25 +11,16 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == p || root == q) return root;
-        List<TreeNode> path1 = new ArrayList<>();
-        List<TreeNode> path2 = new ArrayList<>();
-        lca(root, p, path1);
-        lca(root, q, path2);
-        int i = 0;
-        while(i < path1.size() && i < path2.size() && path1.get(i) == path2.get(i))
-            i++;
-        return i >= 0 ? path1.get(i-1):null;
+        return lca(root, p, q);
         
     }
     
-    private void lca(TreeNode root, TreeNode p, List<TreeNode> path) {
-        if(root == null) return;
-        
-        path.add(root);
-        if(p == root) return;
-        if(root.val > p.val)
-            lca(root.left, p, path);
-        else
-            lca(root.right, p, path);
+    private TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val) {
+            return lca(root.left, p, q);
+        } else if(root.val < p.val && root.val < q.val) {
+            return lca(root.right, p, q);
+        }
+        return root;
     }
 }
