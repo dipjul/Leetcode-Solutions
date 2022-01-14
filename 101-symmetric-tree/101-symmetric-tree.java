9b613662a27b3d2d40fe01319cf8e1ea54b70f79
Dipjul <dipjul25@gmail.com>
2022-01-14 23:35:10 +0530

    Time: 0 ms (100.00%), Space: 37.3 MB (54.89%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -16,15 +16,36 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        return helper(root.left, root.right);
+//         Stack<TreeNode> stack = new Stack<>();
+//         stack.push(root.left);
+//         stack.push(root.right);
+        
+//         while(!stack.empty()) {
+//             TreeNode n1 = stack.pop();
+//             TreeNode n2 = stack.pop();
+//             if(n1 == null && n2 == null) continue;
+//             if(n1 == null || n2 == null || n1.val != n2.val) return false;
+//             stack.push(n1.left);
+//             stack.push(n2.right);
+//             stack.push(n1.right);
+//             stack.push(n2.left);
+//         }
+//        return true;
+        return isInverted(root.left, root.right);
     }
     
-    public boolean helper(TreeNode leftSubtree, TreeNode rightSubtree) {
-        if(leftSubtree == null && rightSubtree == null)
-            return true;
-        if(leftSubtree != null && rightSubtree != null) {
-            return leftSubtree.val == rightSubtree.val && helper(leftSubtree.right, rightSubtree.left) && helper(leftSubtree.left, rightSubtree.right);
-        }
-        return false;
+    public boolean isInverted(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        return p.val == q.val && isInverted(p.left, q.right) && isInverted(p.right, q.left);
     }
+    
+    // public boolean helper(TreeNode leftSubtree, TreeNode rightSubtree) {
+    //     if(leftSubtree == null && rightSubtree == null)
+    //         return true;
+    //     if(leftSubtree != null && rightSubtree != null) {
+    //         return leftSubtree.val == rightSubtree.val && helper(leftSubtree.right, rightSubtree.left) && helper(leftSubtree.left, rightSubtree.right);
+    //     }
+    //     return false;
+    // }
 }
