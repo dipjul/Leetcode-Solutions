b38635a19268465f8e7c6eaa7a17269b3e3fe470
Dipjul <dipjul25@gmail.com>
2022-01-18 22:23:18 +0530

    Time: 16 ms (57.88%), Space: 41.5 MB (60.98%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -8,6 +8,60 @@
  * }
  */
 public class Codec {
+    
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root == null) return "";
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            if(curr == null) {
+                sb.append("$ ");
+                continue;
+            }
+            sb.append(curr.val + " ");
+            q.offer(curr.left);
+            q.offer(curr.right);
+        }
+        return sb.toString();
+    }
+    
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data == "") return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        String[] values = data.trim().split(" ");
+        TreeNode root = new TreeNode(Integer.valueOf(values[0]));
+        q.offer(root);
+        for(int i = 1; i < values.length; i++) {
+            TreeNode parent = q.poll();
+            if(!values[i].equals("$")) {
+                TreeNode left = new TreeNode(Integer.valueOf(values[i]));
+                parent.left = left;
+                q.offer(left);
+            }
+            if(!values[++i].equals("$")) {
+                TreeNode right = new TreeNode(Integer.valueOf(values[i]));
+                parent.right = right;
+                q.offer(right);
+            }
+        }
+        return root;
+        
+    }
+    
+
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+/*
+public class Codec {
 
     // Encodes a tree to a single string.
     static final int EMPTY = 1001;
@@ -50,8 +104,4 @@ public class Codec {
         return root;
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
+*/
