f03e44f2f637fbea0df236f90aefd3fc9e87aa7d
Dipjul <dipjul25@gmail.com>
2022-01-18 22:59:09 +0530

    Time: 3 ms (10.07%), Space: 39.5 MB (24.64%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    /*
     public void flatten(TreeNode root) {
         if(root == null)
             return;
@@ -30,7 +31,7 @@ class Solution {
             curr = curr.right;
         }
     }
-    /*
+    */
     public void flatten(TreeNode root) {
         if(root == null) return;
         Stack<TreeNode> st = new Stack<>();
@@ -47,6 +48,6 @@ class Solution {
             if(curr.left != null) st.push(curr.left);
         }
     }
-    */
+    
 
 }
