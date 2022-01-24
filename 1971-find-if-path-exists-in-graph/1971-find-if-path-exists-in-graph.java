0aa7e241b0dc3ab163e9cecf0528b37460bdf4f7
Dipjul <dipjul25@gmail.com>
2022-01-24 11:02:34 +0530

    Time: 101 ms (74.34%), Space: 164.6 MB (56.59%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean result = false;
+        boolean[] visited = new boolean[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int i = 0; i < edges.length; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(i == source && visited[i] == false) {
+                result = bfs(source, destination, adj, visited);
+            }
+        }
+        return result;
+    }
+    
+    private boolean bfs(int vertex, int dest, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        if(vertex == dest) return true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(vertex);
+        visited[vertex] = true;
+        
+        while(!q.isEmpty()) {
+            int sz = q.size();
+            for(int i = 0; i < sz; i++) {
+                int curr = q.poll();
+                ArrayList<Integer> children = adj.get(curr);
+
+                for(int j = 0; j < children.size(); j++) {
+                    int child = children.get(j);
+                    if(child == dest) return true;
+                    if(visited[child] == false) {
+                        
+                        visited[child] = true;
+                        q.offer(child);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
