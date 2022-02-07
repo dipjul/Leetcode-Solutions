3fccce79ec187f1ba3edd97f9d5b37a1d1ae82a8
Dipjul <dipjul25@gmail.com>
2022-02-07 21:59:38 +0530

    Time: 25 ms (12.46%), Space: 51 MB (5.61%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,48 +1,43 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<Integer> result = new ArrayList<>();
-        // a. Initialize the graph
-        Map<Integer, List<Integer>> graph = new HashMap();
-        Map<Integer, Integer> indegree = new HashMap();
+        
+        // 1. Initialize the graph
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, Integer> inDegree = new HashMap<>();
         
         for(int i = 0; i < numCourses; i++) {
-            graph.put(i, new ArrayList());
-            indegree.put(i, 0);
+            graph.put(i, new ArrayList<>());
+            inDegree.put(i, 0);
         }
         
-        // b. Build the graph
+        // 2. Build the graph
         for(int i = 0; i < prerequisites.length; i++) {
-            int parent = prerequisites[i][1];
-            int child = prerequisites[i][0];
+            int child = prerequisites[i][0], parent = prerequisites[i][1];
             graph.get(parent).add(child);
-            indegree.put(child, indegree.get(child)+1);
+            inDegree.put(child, inDegree.get(child)+1);
         }
         
-        // c. Find all sources i.e., all vertices with 0 in-degrees
+        // 3. Add all the sources(i.e, vertices with in-degree 0) to a queue
         Queue<Integer> sources = new LinkedList<>();
-        for(Map.Entry<Integer, Integer> entry: indegree.entrySet()) {
+        for(Map.Entry<Integer, Integer> entry: inDegree.entrySet())
             if(entry.getValue() == 0)
-                sources.add(entry.getKey());
-        }
+                sources.offer(entry.getKey());
         
-        // d. For each source, add it to the sortedOrder and subtract one from all of its children's in-degrees
-        // if a child's in-degree becomes zero, add it to the sources queue
-        int ind = 0;
+        // 4. For each source, add it to the result, subtract 1 from all of it's children's in-degree
+        // & add if any child has in-degree 0, add it to sources queue
         while(!sources.isEmpty()) {
             int vertex = sources.poll();
             result.add(vertex);
-            List<Integer> children = graph.get(vertex);
-            for(int child: children) {
-                indegree.put(child, indegree.get(child)-1);
-                if(indegree.get(child) == 0)
-                    sources.add(child);
+            for(int child:graph.get(vertex)) {
+                inDegree.put(child, inDegree.get(child)-1);
+                if(inDegree.get(child) == 0)
+                    sources.offer(child);
             }
         }
+        
         if(result.size() != numCourses)
             return new int[]{};
-        int[] res = new int[numCourses];
-        for(int i = 0; i < numCourses; i++)
-            res[i] = result.get(i);
-        return res;
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
