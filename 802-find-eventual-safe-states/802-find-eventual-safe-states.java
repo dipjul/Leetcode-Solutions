c75b3b5276d2852e523fbe97e7fc24b12863b389
Dipjul <dipjul25@gmail.com>
2022-02-07 23:41:36 +0530

    Time: 107 ms (15.88%), Space: 92 MB (20.04%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -1,5 +1,54 @@
 class Solution {
+    /*
+    parent(vertices from which the edges are coming to the current vertex) - parentVertices
+        0:<3>
+        1:<0>
+        2:<0,1>
+        3:<1>
+        4:
+        5:<2,4>
+        6:
+    out-degree
+        0:2
+        1:2
+        2:1
+        3:1
+        4:1
+        5:0
+        6:0
+    q: [5, 6]
+    [5] : -> pq: [5]
+        5:<2,4>
+        out-degree:
+        0:2
+        1:2
+        2:1-1 = 0
+        3:1
+        4:1-1 = 0
+        5:0
+        6:0
+    q: [6, 2, 4]
+    [6] : -> pq: [5, 6]
+        6:
+        out-degree: no change
+    q: [2, 4]
+    [2] : -> pq: [2, 5, 6]
+        2:<0,1>
+        out-degree:
+        0:2-1=1
+        1:2-1=1
+        2:0
+        3:1
+        4:0
+        5:0
+        6:0
+    q: [4]
+    [4] : -> pq: [1, 4, 5, 6]
+        4:
+        out-degree: no change
+    */
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        // to store the result in sorted order
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->a-b);
         
         // Initialize the graph
@@ -18,12 +67,13 @@ class Solution {
             }
         }
         
-        // Add the sources
+        // Add the sources(i.e, vertices with out-degree 0)
         Queue<Integer> q = new LinkedList<>();
         for(Map.Entry<Integer, Integer> entry: outdegree.entrySet())
             if(entry.getValue() == 0)
                 q.offer(entry.getKey());
         
+        // for each source, remove it and add it to the PQ, all the vertices having edge to the source should be           // remove (i.e, subtract one from the out-degree)
         while(!q.isEmpty()) {
             int vertex = q.poll();
             pq.offer(vertex);
@@ -35,6 +85,7 @@ class Solution {
             }
         }
         List<Integer> result = new ArrayList<>();
+        
         while(!pq.isEmpty())
             result.add(pq.poll());
         return result;
