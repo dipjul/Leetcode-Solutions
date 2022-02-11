a80d4f286182a2223af597da04fa484f9856770a
Dipjul <dipjul25@gmail.com>
2022-02-12 02:29:04 +0530

    Time: 9 ms (59.99%), Space: 58.1 MB (9.01%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, n = matrix.length, m = matrix[0].length, col = m - 1;
+        while(row < n && col >= 0) {
+            
+            if(matrix[row][col] == target)
+                return true;
+            else if(matrix[row][col] < target)
+                row++;
+            else
+                col--;
+        }
+        return false;
+    }
+}
