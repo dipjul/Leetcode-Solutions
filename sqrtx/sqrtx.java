cb1b768b5d2b6167e8c2b25e6d257f089bf3c104
Dipjul <dipjul25@gmail.com>
2022-02-12 00:03:44 +0530

    Time: 2 ms, Memory: 40.7 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,26 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x == 0) return 0;
-        if(x == 1) return 1;
-        long start = 0, end = x/2 + 1;
+        int start = 1, end = x, res = 0;
         while(start <= end) {
-            long mid = start + ((end-start)>>1);
-            if(square(mid) > x) {
-                end = mid-1;
-            }
-            else if(square(mid) < x) {
-                if(square(mid+1) > x)
-                    return (int)mid;
-                start = mid+1;
-            }
+            int mid = start + (end - start)/2;
             
-            else
-                return (int)mid;
+            if(mid == x/mid)
+                return mid;
+            else if(mid < x/mid) {
+                res = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-        return -1;
-    }
-    
-    private long square(long x) {
-        return x*x;
+        return res;
     }
 }
