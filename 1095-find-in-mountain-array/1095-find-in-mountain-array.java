ab5c850be8f440d5911ace76f95c41f1069cbc36
Dipjul <dipjul25@gmail.com>
2022-02-12 01:21:40 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (21.34%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,84 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n =  mountainArr.length();
+        int peakElement = findPeak(mountainArr, n);
+        int left = binarySearch(mountainArr, target, 0, peakElement);
+        int right = reverseBinarySearch(mountainArr, target, peakElement+1, mountainArr.length()-1);
+        if(left != -1)
+            return left;
+        else if(right != -1)
+            return right;
+        
+        return -1;
+    }
+    
+    private int binarySearch(MountainArray mountainArr, int target, int start, int end) {
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            int midElement = mountainArr.get(mid);
+            if(midElement == target)
+                return mid;
+            else if(midElement < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+    
+    private int reverseBinarySearch(MountainArray mountainArr, int target, int start, int end) {
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            int midElement = mountainArr.get(mid);
+            if(midElement == target)
+                return mid;
+            else if(midElement < target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+    
+    private int findPeak(MountainArray mountainArr, int n) {
+        int start = 0, end = n - 1;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            int midElement = mountainArr.get(mid);
+            int midElementLeft = mid-1>=0?mountainArr.get(mid-1):-1;   
+            int midElementRight = mid+1<n?mountainArr.get(mid+1):-1; 
+            if(mid > 0 && mid < n-1) {
+
+                if(midElement>midElementRight && midElement>midElementLeft)
+                    return mid;
+                else if(midElementLeft>midElement)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            else if(mid == 0) {
+                if(midElement>midElementRight)
+                    return mid;
+                else
+                    return mid+1;
+            }
+            else if(mid == n-1) {
+                if(midElement>midElementLeft)
+                    return mid;
+                else
+                    return mid-1;
+            }
+        }
+        return -1;
+    }
+}
