7ce371ca01ab0b3e789e9448c1b06ba95add7931
Dipjul <dipjul25@gmail.com>
2022-02-22 17:11:53 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+class Rat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++) a[i][j] = sc.nextInt();
+
+            Solution obj = new Solution();
+            ArrayList<String> res = obj.findPath(a, n);
+            Collections.sort(res);
+            if (res.size() > 0) {
+                for (int i = 0; i < res.size(); i++)
+                    System.out.print(res.get(i) + " ");
+                System.out.println();
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// m is the given matrix and n is the order of matrix
+class Solution {
+    //            U  R  D   L
+    // private static int[] dirR = {-1, 0, 1, 0};
+    // private static int[] dirC = {0, 1, 0, -1};
+    public static ArrayList<String> findPath(int[][] m, int n) {
+        ArrayList<String> res = new ArrayList<>();
+        int[][] vis = new int[n][n];
+        if(m[0][0] == 0 || m[n-1][n-1] == 0)
+            return res;
+        solve(0, 0, m, n, res, "", vis);
+        return res;
+    }
+    
+    private static void solve(int i, int j, int[][] a, int n, ArrayList<String> ans, String move,  int[][] vis) {
+        if(i == n-1 && j == n-1) {
+            ans.add(move);
+            return;
+        }
+        
+        // downward
+        if(i+1<n && vis[i+1][j] == 0 && a[i+1][j] == 1) {
+            vis[i][j] = 1; 
+            solve(i+1, j, a, n, ans, move + 'D', vis);
+            vis[i][j] = 0; 
+        }
+        
+        // left
+        if(j-1>=0 && vis[i][j-1] == 0 && a[i][j-1] == 1) {
+            vis[i][j] = 1; 
+            solve(i, j-1, a, n, ans, move + 'L', vis);
+            vis[i][j] = 0; 
+        }
+        
+        // right 
+        if(j+1<n && vis[i][j+1] == 0 && a[i][j+1] == 1) {
+            vis[i][j] = 1; 
+            solve(i, j+1, a, n, ans, move + 'R', vis);
+            vis[i][j] = 0; 
+        }
+        
+        // upward
+        if(i-1>=0 && vis[i-1][j] ==0 && a[i-1][j] == 1) {
+            vis[i][j] = 1; 
+            solve(i-1, j, a, n, ans, move + 'U', vis);
+            vis[i][j] = 0; 
+        }
+
+    }
+    
+}
