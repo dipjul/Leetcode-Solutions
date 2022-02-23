5e55415a84f2e32a61b0f52d8d367af7f0eeacae
Dipjul <dipjul25@gmail.com>
2022-02-23 10:34:42 +0530

    Time: 0 ms (100.00%), Space: 61.6 MB (6.78%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,35 +1,38 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int start = 0, n = nums.length, end = n-1;
-        // Boundary check
+        int start = 0, end = nums.length-1;
         if(end == 0)
             return nums[0];
-        if(nums[0] != nums[1])
-            return nums[0];
-        if(nums[n-1] != nums[n-2])
-            return nums[n-1];
+        if(nums[start] != nums[start+1])
+            return nums[start];
+        if(nums[end] != nums[end-1])
+            return nums[end];
         
         while(start <= end) {
             int mid = start + (end - start)/2;
+            
             // unique element property
-            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
                 return nums[mid];
-            // partition property
-            else if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1]))
-                start = mid+1;
-            else
+            
+            // partition and pair index property
+            else if((mid%2 == 0 && nums[mid] == nums[mid-1]) ||(mid%2 == 1 && nums[mid] == nums[mid+1]))
                 end = mid-1;
+            else
+                start = mid+1;
+            
         }
         return -1;
     }
 }
+
 /*
-    Partition property and Pair Index property
-    [1, 1, 2, 3, 3, 4, 4, 6, 6]
-     m2 m1
-        mid(m1) = odd && A[mid]==A[mid-1] -> element not present in left side
-        mid(m2) = even && A[mid]==A[mid+1] -> element not present in left side
+Partition property and Pair Index property
+ [1,1,2,3,3,4,4,8,8]
+          m
+m is even & nums[m] == nums[m-1] -> search on left
 
-
-
+ [1,1,2,3,3,4,4]
+        m
+m is odd & nums[m] == nums[m+1] -> search on left
 */
