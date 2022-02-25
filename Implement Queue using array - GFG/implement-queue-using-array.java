2e35b14977ee02b40618636f89ee3032e6f359e7
Dipjul <dipjul25@gmail.com>
2022-02-25 21:01:09 +0530

    Added solution - LeetHub

--- Implement Queue using array - GFG/implement-queue-using-array.java
+++ Implement Queue using array - GFG/implement-queue-using-array.java
@@ -42,31 +42,48 @@ class GfG
 
 class MyQueue {
 
-    int front, rear;
+    int front, rear, size;
 	int arr[] = new int[100005];
 
     MyQueue()
 	{
-		front=0;
-		rear=0;
+		front=-1;
+		rear=-1;
+		size=0;
 	}
 	
 	//Function to push an element x in a queue.
 	void push(int x)
 	{
 	    // Your code here
+	    if(size == 100005)
+	        return;
+	    if(rear==-1) {
+	        front = 0;
+	        rear = 0;
+	    } else {
+	        rear = (rear+1)%100005;
+	    }
 	    arr[rear] = x;
-	    rear++;
+	    size++;
 	} 
 
     //Function to pop an element from queue and return that element.
 	int pop()
 	{
 		// Your code here
-		if(front==rear)
+		if(front==-1)
+		    return -1;
+		if(size == 0)
 		    return -1;
 		int val = arr[front];
-		front++;
+		if(size == 1) {
+		    front=-1;
+		    rear=-1;
+		} else {
+		front = (front+1)%100005;
+		}
+		size--;
 		return val;
 	} 
 }
