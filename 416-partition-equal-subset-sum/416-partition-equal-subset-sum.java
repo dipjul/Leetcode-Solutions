5e10ac6c000dfcfdfe0b56bad43ba330108a0e9d
Dipjul <dipjul25@gmail.com>
2022-02-26 20:26:31 +0530

    Time: 125 ms (17.46%), Space: 72.8 MB (18.03%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -7,8 +7,9 @@ class Solution {
         if(sum%2 != 0)
             return false;
         int target = sum/2;
-        Boolean[][] memo = new Boolean[nums.length][target+1];
-        return helper(nums, target, 0, memo);
+        // Boolean[][] memo = new Boolean[nums.length][target+1];
+        // return helper(nums, target, 0, memo);
+        return helperTopDown(nums, target);
     }
     
     private boolean helper(int[] nums, int target, int index, Boolean[][] memo) {
@@ -20,5 +21,27 @@ class Solution {
             return memo[index][target];
         memo[index][target] = helper(nums, target-nums[index], index+1, memo) || helper(nums, target, index+1, memo);
         return memo[index][target];
+    }
+    
+    private boolean helperTopDown(int[] nums, int target) {
+        Boolean[][] dp = new Boolean[nums.length+1][target+1];
+        
+        for(int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = false;
+        }
+        
+        for(int i = 0; i < dp.length; i++) {
+            dp[i][0] = true;
+        }
+        
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[i].length; j++) {
+                if(j-nums[i-1] >= 0)
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
