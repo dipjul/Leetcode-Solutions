cf7e92ad43991bb612307ca5966bea12dbb20b2d
Dipjul <dipjul25@gmail.com>
2022-02-28 22:16:38 +0530

    Time: 3 ms (77.43%), Space: 42.9 MB (25.22%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,36 +1,33 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        
         int freshCount = 0;
-        Queue<int[]> q = new LinkedList<>();
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        Queue<int[]> rottens = new LinkedList<>();
+        
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == 1)
                     freshCount++;
                 if(grid[i][j] == 2)
-                    q.offer(new int[]{i, j});
+                    rottens.offer(new int[]{i, j});
             }
         }
         
         if(freshCount == 0) return 0;
-        int directions[][] = {{-1,0}, {0, 1}, {1, 0}, {0, -1}};
+        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         int time = 0;
-        while(!q.isEmpty()) {
-            int levelSize = q.size();
+        while(!rottens.isEmpty()) {
+            int levelSize = rottens.size();
             for(int i = 0; i < levelSize; i++) {
-                int[] pos = q.poll();
-                int x = pos[0];
-                int y = pos[1];
-                for(int[] dir:directions) {
-                    int n_x = x + dir[0];
-                    int n_y = y + dir[1];
-                    
-                    if(n_x >= 0 && n_x < m && n_y >= 0 && n_y < n && grid[n_x][n_y] == 1) {
-                        grid[n_x][n_y] = 2;
-                        q.offer(new int[]{n_x, n_y});
-                        freshCount--;
+                int[] coordinate = rottens.poll();
+                for(int[] dir:dirs) {
+                    int nX = coordinate[0]+dir[0];
+                    int nY = coordinate[1]+dir[1];
+                    if(nX>=0 && nX < grid.length && nY >= 0 && nY < grid[0].length) {
+                        if(grid[nX][nY] == 1) {
+                            grid[nX][nY] = 2;
+                            rottens.offer(new int[]{nX, nY});
+                            freshCount--;
+                        }
                     }
                 }
             }
