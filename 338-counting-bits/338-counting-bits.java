5b23aaea095075efe361c14b7270b1b2c21180c4
Dipjul <dipjul25@gmail.com>
2022-04-09 00:06:56 +0530

    Time: 3 ms (46.52%), Space: 48.8 MB (9.18%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] countBits(int n) {
         int[] dp = new int[n+1];
         
-        int offset = 1;
-        
         for(int i = 1; i < n+1; i++) {
-            if(offset * 2 == i)
-                offset = i;
-            dp[i] = 1 + dp[i-offset];
+
+            dp[i] = dp[i/2] + i % 2;
         }
         
         return dp;
     }
 }
+/*
+f[i] = f[i / 2] + i % 2.
+*/
