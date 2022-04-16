929f6c9ad4b6e4715800aea9ba37f7de4adfac05
Dipjul <dipjul25@gmail.com>
2022-04-16 20:12:13 +0530

    Time: 1 ms (83.10%), Space: 41.5 MB (90.19%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -3,8 +3,8 @@ public class Solution {
     public int reverseBits(int n) {
         int res = 0;
         for(int i = 0; i < 32; i++) {
-            int bit = (n >> i) & 1;
-            res = res | (bit << (31 - i));
+            int bit = (n >> i) & 1; // check whether the bit is set or not
+            res = res | (bit << (31 - i)); // put the bit at 31 - i th position i.e, reverse
         }
         return res;
     }
