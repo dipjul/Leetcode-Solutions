692a36858c0933b2b91d8f81874faa501c932dad
Dipjul <dipjul25@gmail.com>
2022-04-20 11:59:08 +0530

    Time: 5 ms (89.01%), Space: 41.8 MB (97.16%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,38 +1,40 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int windowSize = s1.length();
-        StringBuilder sb = new StringBuilder();
-        for(int windowEnd = 0; windowEnd < s2.length(); windowEnd++) {
-            char ch = s2.charAt(windowEnd);
-            if(windowEnd < windowSize)
-                sb.append(ch);
-            else {
-                if(isPermutation(s1, sb.toString()))
-                    return true;
-                sb.deleteCharAt(0);
-                sb.append(ch);
-            }
-            
-        }
-        return isPermutation(s1, sb.toString());
-    }
-    
-    private boolean isPermutation(String s1, String s2) {
-        if(s1.length() != s2.length())
+        if(s1.length() > s2.length())
             return false;
-        char[] arr = new char[26];
+        char[] s1Map = new char[26];
+        char[] s2Map = new char[26];
+        
         for(int i = 0; i < s1.length(); i++) {
-            int ind1 = s1.charAt(i)-'a';
-            int ind2 = s2.charAt(i)-'a';
-            arr[ind1]++;
-            arr[ind2]--;
+            s1Map[s1.charAt(i)-'a']++;
+            s2Map[s2.charAt(i)-'a']++;
         }
-        
+            
+        int matches = 0;
         for(int i = 0; i < 26; i++) {
-            if(arr[i] != 0)
-                return false;
+            if(s1Map[i] == s2Map[i])
+                matches++;
+        }
+        int windowStart = 0;
+        for(int windowEnd = s1.length(); windowEnd < s2.length(); windowEnd++) {
+            if(matches == 26) return true;
+            int rightCharInd = s2.charAt(windowEnd)-'a';
+            s2Map[rightCharInd]++;
+            if(s1Map[rightCharInd] == s2Map[rightCharInd])
+                matches++;
+            else if(s1Map[rightCharInd] + 1 == s2Map[rightCharInd])
+                matches--;
+            
+            int leftCharInd = s2.charAt(windowStart)-'a';
+            s2Map[leftCharInd]--;
+            if(s1Map[leftCharInd] == s2Map[leftCharInd])
+                matches++;
+            else if(s1Map[leftCharInd] - 1 == s2Map[leftCharInd])
+                matches--;
+            
+            windowStart++;
         }
         
-        return true;
+        return matches == 26;
     }
 }
