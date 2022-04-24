db25bdf0e0eb02a3be5518e545d73be020d41301
Dipjul <dipjul25@gmail.com>
2022-04-24 13:29:07 +0530

    Time: 113 ms (88.98%), Space: 61.1 MB (89.42%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -3,8 +3,8 @@ class MedianFinder {
     PriorityQueue<Integer> smallList;
     PriorityQueue<Integer> largeList;
     public MedianFinder() {
-        smallList = new PriorityQueue<>(Collections.reverseOrder());
-        largeList = new PriorityQueue<>();
+        smallList = new PriorityQueue<>(Collections.reverseOrder()); // maxHeap
+        largeList = new PriorityQueue<>(); // minHeap
     }
     
     public void addNum(int num) {
@@ -13,7 +13,7 @@ class MedianFinder {
             largeList.offer(num);
         else
             smallList.offer(num);
-        
+        // size balance
         if(smallList.size() > largeList.size()+1) {
             int tmp = smallList.poll();
             largeList.offer(tmp);
@@ -27,10 +27,8 @@ class MedianFinder {
     public double findMedian() {
 
         if(size % 2 != 0) {
-            // System.out.println("Odd "+"a:"+a+", b:"+b);
             return (double) smallList.peek();
         } else {
-            // System.out.println("Even "+"a:"+a+", b:"+b);
             return (double) (smallList.peek() + largeList.peek())/2.0;
         }
     }
