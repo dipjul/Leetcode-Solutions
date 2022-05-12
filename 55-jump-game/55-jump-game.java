4c974cb5e783186ded84402f9a0956d76857ca40
Dipjul <dipjul25@gmail.com>
2022-05-12 11:44:03 +0530

    Time: 2 ms (86.41%), Space: 68.2 MB (39.68%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int ans = 0, n = nums.length;
+        for(int i = 0; i < n-1; i++) {
+            if(ans < i)
+                return false;
+            ans = Math.max(ans, i+nums[i]);
+            if(ans >= n-1)
+                return true;
+        }
+        return ans >= n-1;
+    }
+}
