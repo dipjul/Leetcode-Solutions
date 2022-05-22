f926f5a8be6d367c5ff62c94fe9b73dcadfd72c1
Dipjul <dipjul25@gmail.com>
2022-05-22 12:57:49 +0530

    Time: 4 ms (45.26%), Space: 47.7 MB (30.86%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,26 +1,30 @@
 class Solution {
+    int tmp;
     public int maxAreaOfIsland(int[][] grid) {
         boolean[][] mark = new boolean[grid.length][grid[0].length];
-        int res = 0;
+        int[] res = {0};
 
-       for (int i = 0; i < grid.length; i++) {
-           for (int j = 0; j < grid[0].length; j++) {
-               if (!mark[i][j] && grid[i][j] == 1) {
-                    res = Math.max(res, dfs(grid, i, j, mark));
-               }
-           }
-       }
-        return res;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (!mark[i][j] && grid[i][j] == 1) {
+                    tmp = 0;
+                    dfs(grid, i, j, mark, res);
+                }
+            }
+        }
+        return res[0];
     }
 
-    private int dfs(int[][] grid, int i, int j, boolean[][] mark) {
+    private void dfs(int[][] grid, int i, int j, boolean[][] mark, int[] res) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || mark[i][j] || grid[i][j] == 0) {
-            return 0;
+            res[0] = Math.max(res[0], tmp);
+            return;
         }
         mark[i][j] = true;
-        return (1 + dfs(grid, i + 1, j, mark) +
-        dfs(grid, i, j - 1, mark) +
-        dfs(grid, i - 1, j, mark) +
-        dfs(grid, i, j + 1, mark));
+        tmp++;
+        dfs(grid, i + 1, j, mark, res);
+        dfs(grid, i, j - 1, mark, res);
+        dfs(grid, i - 1, j, mark, res);
+        dfs(grid, i, j + 1, mark, res);
     }
 }
