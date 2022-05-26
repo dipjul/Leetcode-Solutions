73172a1cff5ed3660fb1b97b162f8ce15b930583
Dipjul <dipjul25@gmail.com>
2022-05-26 19:41:54 +0530

    Time: 1 ms (99.35%), Space: 47.5 MB (60.99%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -5,9 +5,8 @@ class Solution {
 
         List<int[]> mergedList = new ArrayList<>();
         int index = 0;
-        while (index < intervals.length && intervals[index][1] < newInterval[0]) {
-            mergedList.add(intervals[index++]);
-        }
+        while (index < intervals.length && intervals[index][1] < newInterval[0]) mergedList.add(intervals[index++]);
+
         while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
