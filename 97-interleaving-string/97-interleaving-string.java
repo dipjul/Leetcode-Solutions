f825a095a3bbb38118099e938930e7f26710acb3
Dipjul <dipjul25@gmail.com>
2022-05-31 19:17:04 +0530

    Time: 22 ms (10.18%), Space: 49.4 MB (8.23%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -6,22 +6,24 @@ class Solution {
     }
     
     private boolean backtrack(String s1, String s2, String s3) {
-        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
-        if(l3 == l1+l2 && l3 == 0)
+        if(s3.length() == s1.length()+s2.length() && s3.length() == 0)
             return true;
-        if(l3 != l1+l2)
+        if(s3.length() != s1.length()+s2.length())
             return false;
-        String key = l1+","+l2+","+l3;
+        String key = s1.length()+","+s2.length();
         if(mp.containsKey(key))
             return mp.get(key);
         boolean b1 = false, b2 = false;
         
-        if(l1 > 0 && l3 > 0 && s1.charAt(0) == s3.charAt(0))
+        if(s1.length() > 0 && s3.length() > 0 && s1.charAt(0) == s3.charAt(0))
             b1 = backtrack(s1.substring(1), s2, s3.substring(1));
-        if(l2 > 0 && l3 > 0 && s2.charAt(0) == s3.charAt(0))
+        if(s2.length() > 0 && s3.length() > 0 && s2.charAt(0) == s3.charAt(0))
             b2 = backtrack(s1, s2.substring(1), s3.substring(1));
         boolean res = b1 || b2;
         mp.put(key, res);
         return mp.get(key);
     }
 }
+/*
+s1.length, s2.length, s3.length -> value
+*/
