e3b70947d3c0a4092635864bdde3c3ced8c361d7
Dipjul <dipjul25@gmail.com>
2022-06-02 23:38:04 +0530

    Time: 12 ms (36.81%), Space: 45.8 MB (41.83%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,61 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-        Map<Integer, String> mp = new HashMap<>();
-        mp.put(1,"I");
-        mp.put(4,"IV");
-        mp.put(5,"V");
-        mp.put(9,"IX");
-        mp.put(10,"X");
-        mp.put(40, "XL");
-        mp.put(50, "L");
-        mp.put(90,"XC");
-        mp.put(100,"C");
-        mp.put(400,"CD");
-        mp.put(500,"D");
-        mp.put(900, "CM");
-        mp.put(1000, "M");
+        final String[] syms = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        final int[] vals = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
         StringBuilder sb = new StringBuilder();
+        int i = 0;
         while(num > 0) {
-            if(num >= 1000) {
-                num -= 1000;
-                sb.append(mp.get(1000));
-            } else if(num >= 900) {
-                num -= 900;
-                sb.append(mp.get(900));
-            } else if(num >= 500) {
-                num -= 500;
-                sb.append(mp.get(500));
-            } else if(num >= 400) {
-                num -= 400;
-                sb.append(mp.get(400));
-            } else if(num >= 100) {
-                num -= 100;
-                sb.append(mp.get(100));
-            } else if(num >= 90) {
-                num -= 90;
-                sb.append(mp.get(90));
-            } else if(num >= 50) {
-                num -= 50;
-                sb.append(mp.get(50));
-            } else if(num >= 40) {
-                num -= 40;
-                sb.append(mp.get(40));
-            } else if(num >= 10) {
-                num -= 10;
-                sb.append(mp.get(10));
-            } else if(num >= 9) {
-                num -= 9;
-                sb.append(mp.get(9));
-            } else if(num >= 5) {
-                num -= 5;
-                sb.append(mp.get(5));
-            } else if(num >= 4) {
-                num -= 4;
-                sb.append( mp.get(4));
+            if(num - vals[i] >= 0) {
+                num -= vals[i];
+                sb.append(syms[i]);
             } else {
-                num -= 1;
-                sb.append(mp.get(1));
+                i++;
             }
         }
         
