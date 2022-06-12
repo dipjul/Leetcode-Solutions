30d70f9c2b5d05e08b451ffcf7ced81fd93f4022
Dipjul <dipjul25@gmail.com>
2022-06-12 10:22:06 +0530

    Time: 162 ms (10.74%), Space: 119.2 MB (65.79%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -3,13 +3,14 @@ class Solution {
         Map<Integer,Integer> mp = new HashMap<>();
         int winS = 0, maxSum = 0, sum = 0;
         for(int winE = 0; winE < nums.length; winE++) {
+            sum += nums[winE];
             if(mp.containsKey(nums[winE])) {
+                int i = winS;
                 winS = Math.max(winS, mp.get(nums[winE])+1);
                 mp.remove(nums[winE]);
+                while(i < winS)
+                    sum -= nums[i++];
             }
-            sum = 0;
-            for(int i = winS; i <= winE; i++)
-                sum += nums[i];
             maxSum = Math.max(maxSum, sum);
             mp.put(nums[winE], winE);
         }
