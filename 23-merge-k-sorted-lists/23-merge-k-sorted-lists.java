bea1093f479974828094582d8c9ef0cbdc1a3585
Dipjul <dipjul25@gmail.com>
2022-07-08 11:09:28 +0530

    Time: 2 ms (98.48%), Space: 47.4 MB (51.63%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -9,42 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        int k = lists.length;
-        if(k == 0)
-            return null;
-        int i = 0;
-        while(i+1<k) {
-            ListNode l1 = lists[i], l2 = lists[i+1];
-            lists[i+1] = merge2LL(l1, l2);
-            i++;
-        }
-        return lists[k-1];
+
+    public ListNode partion(ListNode[] lists, int s, int e) {
+        if (s == e) return lists[s];
+        if (s < e) {
+            int q = (s + e) / 2;
+            ListNode l1 = partion(lists, s, q);
+            ListNode l2 = partion(lists, q + 1, e);
+            return merge2LL(l1, l2);
+        } else return null;
     }
-    
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return partion(lists, 0, lists.length - 1);
+    }
+
     public ListNode merge2LL(ListNode l1, ListNode l2) {
-       ListNode dummyHead = new ListNode(0);
-       ListNode tmp = dummyHead;
-       while(l1 != null && l2 != null) {
-           if(l1.val < l2.val) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tmp = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 tmp.next = l1;
                 ListNode nextL1 = l1.next;
                 l1.next = l2;
                 l1 = nextL1;
                 tmp = tmp.next;
-           } else {
+            } else {
                 tmp.next = l2;
                 ListNode nextL2 = l2.next;
                 l2.next = l1;
                 l2 = nextL2;
                 tmp = tmp.next;
-           }
-       }
+            }
+        }
 
-       if(l1 == null)
-           tmp.next = l2;
-       if(l2 == null)
-           tmp.next = l1;
-       return dummyHead.next;
-   }
+        if (l1 == null) tmp.next = l2;
+        if (l2 == null) tmp.next = l1;
+        return dummyHead.next;
+    }
 }
