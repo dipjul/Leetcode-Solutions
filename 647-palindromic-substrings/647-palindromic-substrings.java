957da54defb4552c07cecb69efc5d2b18ccc310b
Dipjul <dipjul25@gmail.com>
2022-07-29 14:40:40 +0530

    Time: 7 ms (61.50%), Space: 42.2 MB (63.35%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,39 +1,26 @@
 class Solution {
     public int countSubstrings(String s) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("$");
-        for(int i = 0; i < s.length(); i++) {
-            sb.append("#");
-            sb.append(s.charAt(i));
+        if (s.length() < 2) {
+            return s.length();
         }
-        sb.append("#");
-        sb.append("@");
-        int[] p = manacher_odd(sb.toString());
-        int ans = 0;
-        for(int i = 0; i < p.length; i++)
-            ans += (p[i]+1)/2;
-        return ans;
-    }
-    
-    private int[] manacher_odd(String s) {
-        int[] p = new int[s.length()];
-        int center = 0, rightBoundary = 0;
-        
-        for(int i = 1; i < s.length()-1; i++) {
-            
-            int mirror = 2*center-i;
-            if(i < rightBoundary)
-                p[i] = Math.min(p[mirror], rightBoundary-i);
-            
-            while(s.charAt(i-(p[i]+1)) == s.charAt(i+(p[i]+1)))
-                p[i]++;
-            
-            if(i + p[i] > rightBoundary) {
-                center = i;
-                rightBoundary = i + p[i];
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // Odd Length
+            int left = i, right = i;
+            while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                result++;
+                left -=1;
+                right +=1;
+            }
+            // Even Length
+            left = i;
+            right = i + 1;
+            while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                result++;
+                left -=1;
+                right +=1;
             }
         }
-        
-        return p;
+        return result;
     }
 }
